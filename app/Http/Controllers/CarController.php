06840ec97d0e5f4826d9e63c6369ac7a336acadd
carlosefrain123 <carlosefrainchallo1@gmail.com>
2024-01-12 23:08:59 -0500

    12/01/2024 - 23:07 - Se puede visualizar los datos de la base de datos, en el index - Básicamente ya hicimos el ejemplo de MVC

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Car;
+use App\Models\Car;//TODO: Se le coloca el nombre de la clase, del model
 use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
     public function index(){
         $cars=Car::all();
-        return view('cars.index',compact('cars'));
+        return view('car.index',compact('cars'));
     }
 }
---
+++ resources/views/car/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Listado de autos</h1>
+<ul>
+    @foreach($cars as $car)
+    <li>
+        {{ $car->year}} {{ $car->make}} {{ $car->model}} {{ $car->color}}
+    </li>
+    @endforeach
+</ul>
+</body>
+</html>
